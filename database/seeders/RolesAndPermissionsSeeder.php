360a1de7408097aa796b28060ef1d89859db934c
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-09-28 10:54:57 -0600

    feat: Enhance PermissionManager and RolesAndPermissionsSeeder for role-based permissions management

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -27,53 +27,25 @@ class RolesAndPermissionsSeeder extends Seeder
         'resources' => ['download resources'],
     ];
 
+    const ROLES = [
+        'admin' => '*',
+        'student' => [
+            'profiles' => ['read', 'update'],
+            'levels' => ['read'],
+            'units' => ['read'],
+            'resources' => ['read', 'download resources'],
+            'lessons' => ['read'],
+            'exercises' => ['read'],
+            'exercise_types' => ['read'],
+        ],
+    ];
+
     /**
      * Run the database seeds.
      */
     public function run()
     {
         $manager = new PermissionManager(self::PERMISSIONS, self::SPECIAL_PERMISSIONS);
-        $allPermissions = $manager->get();
-
-        $this->createPermissions($allPermissions);
-
-        $this->assignPermissionsToRoles();
-    }
-
-    protected function createPermissions($permissions): void
-    {
-        foreach ($permissions as $perms) {
-            foreach ($perms as $perm) {
-                Permission::firstOrCreate(['name' => $perm]);
-            }
-        }
-    }
-
-    protected function filterPermissions($permission): PermissionManager
-    {
-        $permissions = self::PERMISSIONS[$permission] ?? [];
-        $specialPermissions = self::SPECIAL_PERMISSIONS[$permission] ?? [];
-
-        return new PermissionManager([$permission => $permissions], [$permission => $specialPermissions]);
-    }
-
-    protected function assignPermissionsToRoles(): void
-    {
-        $adminRole = Role::firstOrCreate(['name' => 'admin']);
-        $studentRole = Role::firstOrCreate(['name' => 'student']);
-
-        $adminRole->givePermissionTo(Permission::all());
-
-        $studentPermissions = array_merge(
-            $this->filterPermissions('profiles')->get(),
-            $this->filterPermissions('levels')->only(['read levels'])->get(),
-            $this->filterPermissions('units')->only(['read units'])->get(),
-            $this->filterPermissions('resources')->only(['read resources', 'download resources'])->get(),
-            $this->filterPermissions('lessons')->only(['read lessons'])->get(),
-            $this->filterPermissions('exercises')->only(['read exercises'])->get(),
-            $this->filterPermissions('exercise_types')->only(['read exercise_types'])->get()
-        );
-
-        $studentRole->givePermissionTo($studentPermissions);
+        $manager->withRoles(self::ROLES)->sync();
     }
 }
