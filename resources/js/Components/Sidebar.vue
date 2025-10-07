ead26132021dc2952116015ddae4269450965ec2
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-10-07 14:30:21 -0600

    Implement code changes to enhance functionality and improve performance

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,100 +1,100 @@
-<template>
-    <!-- Overlay for mobile -->
-    <div v-if="modelValue" class="fixed inset-0 z-40 lg:hidden" @click.self="$emit('update:modelValue', false)">
-        <div class="absolute inset-0 bg-black/40"></div>
-    </div>
-
-    <!-- Sidebar -->
-    <aside :class="[
-        'fixed inset-y-0 left-0 z-50 transform bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 shadow-lg lg:shadow-none transition-all duration-200 ease-in-out',
-        modelValue ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
-        'w-64 lg:w-64',
-        props.collapsed ? 'lg:w-16' : ''
-    ]">
-        <div
-            :class="['h-16 flex items-center border-b border-gray-200 dark:border-gray-700 px-4 justify-between', props.collapsed ? 'lg:px-2 lg:justify-start lg:gap-1' : '']">
-            <Link :href="homeHref" class="flex items-center gap-2 overflow-hidden flex-shrink-0">
-            <i class="fa-solid fa-layer-group w-6 text-center text-xl text-gray-800 dark:text-gray-100"></i>
-            <h1 v-show="!effectiveCollapsed"
-                class="text-xl font-bold text-gray-800 dark:text-gray-100 whitespace-nowrap">
-                Learning <span class="text-[#FF2D20]">English</span>
-            </h1>
-            </Link>
-            <div class="flex items-center gap-2">
-                <!-- Desktop collapse toggle -->
-                <button class="hidden lg:inline-flex"
-                    :class="[effectiveCollapsed ? 'p-1.5' : 'p-2', 'text-gray-500 hover:text-gray-700 dark:text-gray-400']"
-                    @click="toggleCollapsed" :title="collapsed ? 'Expandir' : 'Contraer'">
-                    <i :class="collapsed ? 'fa-solid fa-angles-right' : 'fa-solid fa-angles-left'"></i>
-                </button>
-                <!-- Mobile close -->
-                <button class="lg:hidden p-2 text-gray-500 hover:text-gray-700 dark:text-gray-400"
-                    @click="$emit('update:modelValue', false)">
-                    <i class="fa-solid fa-xmark text-lg"></i>
-                </button>
-            </div>
+    <template>
+        <!-- Overlay for mobile -->
+        <div v-if="modelValue" class="fixed inset-0 z-40 lg:hidden" @click.self="$emit('update:modelValue', false)">
+            <div class="absolute inset-0 bg-black/40"></div>
         </div>
 
-        <nav class="p-3 space-y-1 overflow-y-auto h-[calc(100vh-4rem)]">
-            <!-- Admin links -->
-            <template v-if="hasRole('admin')">
-                <SidebarLink :href="route('units.index')" :active="route().current('units.index')"
-                    :compact="effectiveCollapsed" title-text="Unidades">
-                    <i class="fa-solid fa-layer-group w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Unidades</span>
-                </SidebarLink>
-                <SidebarLink :href="route('levels.index')" :active="route().current('levels.index')"
-                    :compact="effectiveCollapsed" title-text="Niveles">
-                    <i class="fa-solid fa-list w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Niveles</span>
-                </SidebarLink>
-                <SidebarLink :href="route('lessons.index')" :active="route().current('lessons.index')"
-                    :compact="effectiveCollapsed" title-text="Lecciones">
-                    <i class="fa-solid fa-book w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Lecciones</span>
-                </SidebarLink>
-                <SidebarLink :href="route('resources.index')" :active="route().current('resources.index')"
-                    :compact="effectiveCollapsed" title-text="Recursos">
+        <!-- Sidebar -->
+        <aside :class="[
+            'fixed inset-y-0 left-0 z-50 transform bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-700 shadow-lg lg:shadow-none transition-all duration-200 ease-in-out',
+            modelValue ? 'translate-x-0' : '-translate-x-full lg:translate-x-0',
+            'w-64 lg:w-64',
+            props.collapsed ? 'lg:w-16' : ''
+        ]">
+            <div
+                :class="['h-16 flex items-center border-b border-gray-200 dark:border-gray-700 px-4 justify-between', props.collapsed ? 'lg:px-2 lg:justify-start lg:gap-1' : '']">
+                <Link :href="homeHref" class="flex items-center gap-2 overflow-hidden flex-shrink-0">
+                <img src="/img/logo03.png" alt="Learning English logo" class="w-10 h-10 object-contain" />
+                <h1 v-show="!effectiveCollapsed"
+                    class="text-xl font-bold text-gray-800 dark:text-gray-100 whitespace-nowrap font-nativo">
+                    N A T I V O
+                </h1>
+                </Link>
+                <div class="flex items-center gap-2">
+                    <!-- Desktop collapse toggle -->
+                    <button class="hidden lg:inline-flex"
+                        :class="[effectiveCollapsed ? 'p-1.5' : 'p-2', 'text-gray-500 hover:text-gray-700 dark:text-gray-400']"
+                        @click="toggleCollapsed" :title="collapsed ? 'Expandir' : 'Contraer'">
+                        <i :class="collapsed ? 'fa-solid fa-angles-right' : 'fa-solid fa-angles-left'"></i>
+                    </button>
+                    <!-- Mobile close -->
+                    <button class="lg:hidden p-2 text-gray-500 hover:text-gray-700 dark:text-gray-400"
+                        @click="$emit('update:modelValue', false)">
+                        <i class="fa-solid fa-xmark text-lg"></i>
+                    </button>
+                </div>
+            </div>
+
+            <nav class="p-3 space-y-1 overflow-y-auto h-[calc(100vh-4rem)]">
+                <!-- Admin links -->
+                <template v-if="hasRole('admin')">
+                    <SidebarLink :href="route('units.index')" :active="route().current('units.index')"
+                        :compact="effectiveCollapsed" title-text="Unidades">
+                        <i class="fa-solid fa-layer-group w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Unidades</span>
+                    </SidebarLink>
+                    <SidebarLink :href="route('levels.index')" :active="route().current('levels.index')"
+                        :compact="effectiveCollapsed" title-text="Niveles">
+                        <i class="fa-solid fa-list w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Niveles</span>
+                    </SidebarLink>
+                    <SidebarLink :href="route('lessons.index')" :active="route().current('lessons.index')"
+                        :compact="effectiveCollapsed" title-text="Lecciones">
+                        <i class="fa-solid fa-book w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Lecciones</span>
+                    </SidebarLink>
+                    <SidebarLink :href="route('resources.index')" :active="route().current('resources.index')"
+                        :compact="effectiveCollapsed" title-text="Recursos">
+                        <i class="fa-solid fa-user w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Recursos</span>
+                    </SidebarLink>
+                    <SidebarLink :href="route('exercises.index')" :active="route().current('exercises.index')"
+                        :compact="effectiveCollapsed" title-text="Ejercicios">
+                        <i class="fa-solid fa-pencil w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Ejercicios</span>
+                    </SidebarLink>
+                    <SidebarLink :href="route('admin.progress.index')" :active="route().current('admin.progress.index')"
+                        :compact="effectiveCollapsed" title-text="Progreso">
+                        <i class="fa-solid fa-chart-line w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Progreso</span>
+                    </SidebarLink>
+                </template>
+
+                <!-- Student links -->
+                <template v-if="hasRole('student')">
+                    <SidebarLink :href="route('student.units.index')" :active="route().current('student.units.index')"
+                        :compact="effectiveCollapsed" title-text="Unidades">
+                        <i class="fa-solid fa-layer-group w-5 text-center"></i>
+                        <span v-show="!effectiveCollapsed">Unidades</span>
+                    </SidebarLink>
+                </template>
+
+                <!-- Divider -->
+                <hr class="my-3 border-gray-200 dark:border-gray-700" />
+
+                <!-- Profile/Logout -->
+                <SidebarLink :href="route('profile.edit')" :compact="effectiveCollapsed" title-text="Perfil">
                     <i class="fa-solid fa-user w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Recursos</span>
+                    <span v-show="!effectiveCollapsed">Perfil</span>
                 </SidebarLink>
-                <SidebarLink :href="route('exercises.index')" :active="route().current('exercises.index')"
-                    :compact="effectiveCollapsed" title-text="Ejercicios">
-                    <i class="fa-solid fa-pencil w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Ejercicios</span>
+                <SidebarLink :href="route('logout')" method="post" as="button" :compact="effectiveCollapsed"
+                    title-text="Cerrar sesión">
+                    <i class="fa-solid fa-right-from-bracket w-5 text-center"></i>
+                    <span v-show="!effectiveCollapsed">Cerrar sesión</span>
                 </SidebarLink>
-                <SidebarLink :href="route('admin.progress.index')" :active="route().current('admin.progress.index')"
-                    :compact="effectiveCollapsed" title-text="Progreso">
-                    <i class="fa-solid fa-chart-line w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Progreso</span>
-                </SidebarLink>
-            </template>
-
-            <!-- Student links -->
-            <template v-if="hasRole('student')">
-                <SidebarLink :href="route('student.units.index')" :active="route().current('student.units.index')"
-                    :compact="effectiveCollapsed" title-text="Unidades">
-                    <i class="fa-solid fa-layer-group w-5 text-center"></i>
-                    <span v-show="!effectiveCollapsed">Unidades</span>
-                </SidebarLink>
-            </template>
-
-            <!-- Divider -->
-            <hr class="my-3 border-gray-200 dark:border-gray-700" />
-
-            <!-- Profile/Logout -->
-            <SidebarLink :href="route('profile.edit')" :compact="effectiveCollapsed" title-text="Perfil">
-                <i class="fa-solid fa-user w-5 text-center"></i>
-                <span v-show="!effectiveCollapsed">Perfil</span>
-            </SidebarLink>
-            <SidebarLink :href="route('logout')" method="post" as="button" :compact="effectiveCollapsed"
-                title-text="Cerrar sesión">
-                <i class="fa-solid fa-right-from-bracket w-5 text-center"></i>
-                <span v-show="!effectiveCollapsed">Cerrar sesión</span>
-            </SidebarLink>
-        </nav>
-    </aside>
-</template>
+            </nav>
+        </aside>
+    </template>
 
 <script setup>
 import { computed, onMounted, onUnmounted, ref } from 'vue';
@@ -148,4 +148,10 @@ const effectiveCollapsed = computed(() => (isDesktop.value ? props.collapsed : f
 
 <style scoped>
 /****/
+@import url('https://fonts.googleapis.com/css2?family=Suez+One&display=swap" rel="stylesheet');
+
+.font-nativo {
+    font-family: 'Suez One';
+    letter-spacing: 0.2em;
+}
 </style>
