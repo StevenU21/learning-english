d312aa6c03985bcd05b33c03115b403aa1b6b39f
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-10-07 14:49:49 -0600

    refactor: Update GuestLayout structure for improved branding and visual consistency

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,10 +4,11 @@ import { Link } from '@inertiajs/vue3';
 
 <template>
     <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
-        <div class="text-center">
+        <div class="text-center flex flex-col items-center mt-4 mb-2">
             <Link href="/">
-            <h1 class="text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Learning <span class="text-[#FF2D20]">English</span>
+            <img src="/img/logo02.png" alt="NATIVO Logo" class="w-16 h-16 sm:w-24 sm:h-24 mx-auto" />
+            <h1 class="text-2xl sm:text-3xl font-nativo text-gray-900 dark:text-white tracking-wide mb-1">
+                <span class="block">N A T I V O</span>
             </h1>
             </Link>
         </div>
@@ -18,3 +19,17 @@ import { Link } from '@inertiajs/vue3';
         </div>
     </div>
 </template>
+
+<style scoped>
+@font-face {
+    font-family: 'SuezOne';
+    src: url('/fonts/SuezOne-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.font-nativo {
+    font-family: 'SuezOne', serif;
+    letter-spacing: 0.08em;
+}
+</style>
