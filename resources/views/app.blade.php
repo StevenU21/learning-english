57be3ef2b299ba216173617a8e2d67159f86398e
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-10-07 21:01:37 +0000

    fix: Update project name in package-lock and correct app title in HTML template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Nativo') }}</title>
 
     <!-- Fonts -->
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/img/logo02.png">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
