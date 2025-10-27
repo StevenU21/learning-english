1038eeef117f6454e4c37abbf4a8b86580c670f0
StevenU21 <ulloadeifheltsteven@gmail.com>
2025-10-27 06:06:47 +0000

    refactor: replace SecondaryButton with Link component in LessonCard and UnitCard for improved consistency and clarity

--- resources/js/Pages/Student/Units/components/UnitCard.vue
+++ resources/js/Pages/Student/Units/components/UnitCard.vue
@@ -56,9 +56,10 @@
                 class="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300">
             <i class="fa-solid fa-play mr-2"></i> Ingresar
             </Link>
-            <SecondaryButton :is="Link" :href="route('student.units.resources.index', { unit: unit.slug })" class="items-center">
+            <Link :href="route('student.units.resources.index', { unit: unit.slug })"
+                class="inline-flex items-center rounded-md border-2 border-gray-200 dark:border-gray-700 bg-white px-4 py-2 text-xs font-semibold uppercase tracking-widest text-gray-700 shadow-sm transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800">
                 <i class="fa-solid fa-book-open mr-2"></i> Recursos
-            </SecondaryButton>
+            </Link>
         </div>
     </div>
 </template>
